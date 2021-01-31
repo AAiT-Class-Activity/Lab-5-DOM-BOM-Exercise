b13334ecb2bd9a8e3f583e32d992743608067994
Eshtaol Girma Bayessa <it.eshtaol.girma@gmail.com>
2021-01-31 16:39:02 +0300

    number writer funnction created

--- Calculator/Assets/js/app.js
+++ Calculator/Assets/js/app.js
@@ -27,3 +27,94 @@ var operatorActive = false;
 var firstNumber = "";
 var operation = "";
 
+// Number input function
+function addNumberOne() {
+  if (number.textContent == 0 || operatorActive == true) {
+    operatorActive = false;
+    number.textContent = 1;
+  } else {
+    number.textContent += 1;
+  }
+}
+function addNumberTwo() {
+  if (number.textContent == 0 || operatorActive == true) {
+    operatorActive = false;
+    number.textContent = 2;
+  } else {
+    number.textContent += 2;
+  }
+}
+function addNumberThree() {
+  if (number.textContent == 0 || operatorActive == true) {
+    operatorActive = false;
+    number.textContent = 3;
+  } else {
+    number.textContent += 3;
+  }
+}
+function addNumberFour() {
+  if (number.textContent == 0 || operatorActive == true) {
+    operatorActive = false;
+    number.textContent = 4;
+  } else {
+    number.textContent += 4;
+  }
+}
+function addNumberFive() {
+  if (number.textContent == 0 || operatorActive == true) {
+    operatorActive = false;
+    number.textContent = 5;
+  } else {
+    number.textContent += 5;
+  }
+}
+function addNumberSix() {
+  if (number.textContent == 0 || operatorActive == true) {
+    operatorActive = false;
+    number.textContent = 6;
+  } else {
+    number.textContent += 6;
+  }
+}
+function addNumberSeven() {
+  if (number.textContent == 0 || operatorActive == true) {
+    operatorActive = false;
+    number.textContent = 7;
+  } else {
+    number.textContent += 7;
+  }
+}
+function addNumberEight() {
+  if (number.textContent == 0 || operatorActive == true) {
+    operatorActive = false;
+    number.textContent = 8;
+  } else {
+    number.textContent += 8;
+  }
+}
+function addNumberNine() {
+  if (number.textContent == 0 || operatorActive == true) {
+    operatorActive = false;
+    number.textContent = 9;
+  } else {
+    number.textContent += 9;
+  }
+}
+function addNumberZero() {
+  if (number.textContent == 0 || operatorActive == true) {
+    operatorActive = false;
+    number.textContent = 0;
+  } else {
+    number.textContent += 0;
+  }
+}
+oneBtn.addEventListener("click", addNumberOne);
+twoBtn.addEventListener("click", addNumberTwo);
+threeBtn.addEventListener("click", addNumberThree);
+fourBtn.addEventListener("click", addNumberFour);
+fiveBtn.addEventListener("click", addNumberFive);
+sixBtn.addEventListener("click", addNumberSix);
+sevenBtn.addEventListener("click", addNumberSeven);
+eightBtn.addEventListener("click", addNumberEight);
+nineBtn.addEventListener("click", addNumberNine);
+zeroBtn.addEventListener("click", addNumberZero);
