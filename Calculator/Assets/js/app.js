95443307ce49aa6116fbae580b4bec8e7be45f02
Eshtaol Girma Bayessa <it.eshtaol.girma@gmail.com>
2021-01-31 16:35:47 +0300

    UI variable decalred and addition variables created

---
+++ Calculator/Assets/js/app.js
@@ -0,0 +1,29 @@
+// Define UI Variables
+const number = document.querySelector(".input-number");
+const clearBtn = document.querySelector(".clear-btn");
+const negativeBtn = document.querySelector(".negative-btn");
+const divideBtn = document.querySelector(".divide-btn");
+const timesBtn = document.querySelector(".times-btn");
+const minusBtn = document.querySelector(".minus-btn");
+const plusBtn = document.querySelector(".plus-btn");
+const equalsBtn = document.querySelector(".equal-btn");
+const decimalBtn = document.querySelector(".decimal-btn");
+const percentBtn = document.querySelector(".percent-btn");
+
+const zeroBtn = document.querySelector(".zero-btn");
+const oneBtn = document.querySelector(".one-btn");
+const twoBtn = document.querySelector(".two-btn");
+const threeBtn = document.querySelector(".three-btn");
+const fourBtn = document.querySelector(".four-btn");
+const fiveBtn = document.querySelector(".five-btn");
+const sixBtn = document.querySelector(".six-btn");
+const sevenBtn = document.querySelector(".seven-btn");
+const eightBtn = document.querySelector(".eight-btn");
+const nineBtn = document.querySelector(".nine-btn");
+
+// control variable
+var operationSettled = "";
+var operatorActive = false;
+var firstNumber = "";
+var operation = "";
+
